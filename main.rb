f74a202df2ef1b49cd52a8c4d0f1103cf492f0de
melkotoury <melkotoury@gmail.com>
2018-07-08 00:10:03 +0200

    filter correct and incorrect

--- main.rb
+++ main.rb
@@ -3,5 +3,13 @@ require 'csv'
 csv_text = File.read('corrupt_reasons.csv')
 csv = CSV.parse(csv_text, :headers => true)
 csv.each do |row|
-  puts(row.to_hash['id'])
+  id = row.to_hash['id']
+  reasons = row.to_hash['retoure_reason']
+  if (reasons.to_i <= 10 && reasons.to_i >= 1) || reasons == 'x1'|| reasons =='x2'|| reasons =='x3' || reasons =='x4'
+  #   data should return the same value as it is correct
+  else
+    corrected = reasons.gsub(/:|\./, ',')
+    puts reasons
+    puts id
+  end
 end
